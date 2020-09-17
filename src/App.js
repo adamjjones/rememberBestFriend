0d992ae4a81a4e516e97d20eee5c5cc9c9b66016
Adam Jones <adam@linuxtampa.com>
2020-09-17 01:29:01 -0400

    Made 4 components and navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import topleft from './images/top-left-img-2.png'
+import topright from './images/top-right-img.png'
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import Order from './components/Order'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+
+          <ul id="nav-mobile">
+            <li><Link to="/App">Home</Link></li>
+            <li><Link to="/Order">Orders</Link></li>
+            <li><Link to="/About">About us</Link></li>
+            <li><Link to="/Cart">Cart</Link></li>
+          </ul>
+          <img src={topleft} />
+          <img src={topright} />
+        </nav>
+        <Switch>
+          <Route path="/Order" exact component={Order} />
+        </Switch>
+      </Router>
     </div>
   );
 }
